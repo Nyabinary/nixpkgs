4425203bab9eaf520294e055c4c75b163aa06230
sntx <sntx.htqjd@simplelogin.com>
2024-01-29 23:24:49 +0100

    nixos/portmaster: init module
    C.f. [portmaster.service](https://github.com/safing/portmaster-packaging/blob/ab204c69380e5a9101a89f382dcfb98fbfc926de/linux/portmaster.service)
    
    portmaster: refactor
    
    Adhere more closely to the contributing guidelines

--- pkgs/by-name/po/portmaster/package.nix
+++ pkgs/by-name/po/portmaster/package.nix
@@ -2,23 +2,25 @@
 , buildGoModule
 , fetchFromGitHub
 , iptables
+,
 }:
-
 buildGoModule rec {
   pname = "portmaster";
   version = "1.6.0";
   CGO_ENABLED = 0;
 
-  ldflags = let
-    BUILD_PATH = "github.com/safing/portbase/info";
-  in [
-    "-X ${BUILD_PATH}.commit=v${version}"
-    "-X ${BUILD_PATH}.buildOptions="
-    "-X ${BUILD_PATH}.buildUser=nixpkgs"
-    "-X ${BUILD_PATH}.buildHost=hydra"
-    "-X ${BUILD_PATH}.buildDate=31.10.2023"
-    "-X ${BUILD_PATH}.buildSource=${src.gitRepoUrl}"
-  ];
+  ldflags =
+    let
+      BUILD_PATH = "github.com/safing/portbase/info";
+    in
+    [
+      "-X ${BUILD_PATH}.commit=v${version}"
+      "-X ${BUILD_PATH}.buildOptions="
+      "-X ${BUILD_PATH}.buildUser=nixpkgs"
+      "-X ${BUILD_PATH}.buildHost=hydra"
+      "-X ${BUILD_PATH}.buildDate=31.10.2023"
+      "-X ${BUILD_PATH}.buildSource=${src.gitRepoUrl}"
+    ];
 
   src = fetchFromGitHub {
     owner = "safing";
@@ -42,6 +44,6 @@ buildGoModule rec {
     description = "A free and open-source application firewall that does the heavy lifting for you";
     homepage = "https://safing.io";
     license = licenses.agpl3;
-    maintainers = with maintainers; [ nyanbinary ];
+    maintainers = with maintainers; [ nyanbinary syntax626 ];
   };
 }
